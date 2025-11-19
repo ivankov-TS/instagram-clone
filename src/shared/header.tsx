c25c483b656514b8e0e4a39a8d780b022d2d70bd
ivankov-TS <tima.ivankov.11_03@mail.ru>
2025-11-19 20:41:08 +0500

    fix: correct header and action icons layout

---
+++ src/shared/header.tsx
@@ -0,0 +1,50 @@
+import {
+  ActionIcon,
+  AppShell,
+  Avatar,
+  Container,
+  Group,
+  Indicator,
+  TextInput,
+} from "@mantine/core";
+import { Logo } from "./ui/logo";
+import { Search, Plus, MessageCircle, Bell } from "lucide-react";
+
+export const Header = () => {
+  return (
+    <AppShell.Header>
+      <Container size="xl" h="100%">
+        <Group h="100%" align="center" justify="space-between">
+          <Logo />
+          <TextInput
+            radius="lg"
+            placeholder="Search..."
+            w={300}
+            size="md"
+            leftSection={<Search size={20} />}
+          />
+          <Group>
+            <ActionIcon radius="xl" size="lg" variant="default">
+              <Plus size={20} />
+            </ActionIcon>
+            <ActionIcon radius="xl" size="lg" variant="default">
+              <MessageCircle size={20} />
+            </ActionIcon>
+            <Indicator size={15} offset={4} withBorder>
+              <ActionIcon radius="xl" size="lg" variant="default">
+                <Bell size={20} />
+              </ActionIcon>
+            </Indicator>
+            <Avatar
+              variant="filled"
+              styles={{ root: { border: "2px solid black" } }}
+              color="black"
+              radius="xl"
+              src="/avatar.png"
+            />
+          </Group>
+        </Group>
+      </Container>
+    </AppShell.Header>
+  );
+};
